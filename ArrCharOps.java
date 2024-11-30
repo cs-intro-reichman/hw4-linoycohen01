3456a64524c7156a79f0d2704bc920e4897c11d5
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-11-30 21:05:46 +0200

    4

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -170,7 +170,14 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        // Replace the following statement with your code
+        int length = Math.min(str1.length(), str2.length());
+        for(int i = 0; i < length; i++){
+            if(str1.charAt(i) < str2.charAt(i)){
+                return -1;
+            }else if(str1.charAt(i) > str2.charAt(i)){
+                return 1;
+            }
+        }
         return 0;
     }
 }
