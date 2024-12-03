6e5343bf7b5daf2bd93a8353190215b24fbc86a2
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-12-03 22:11:10 +0200

    14

--- MyString.java
+++ MyString.java
@@ -1,5 +1,14 @@
 public class MyString {
     public static void main(String[] args) {
+       System.out.println(contains("baba yaga", "baba"));
+       System.out.println(contains("baba yaga", ""));
+       System.out.println((contains("baba yaga", "John Wick is the baba yaga") )); // fale
+      boolean var3 = !MyString.contains("baba yaga", "John Wick is the baba yaga");
+      boolean var4 = !MyString.contains("baba yaga", "Yaga");
+      boolean var5 = MyString.contains("baba yaga", "babayaga"); //זה מחזיר נכון במקום לא נכון
+      System.err.println(var3);
+      System.out.println(var4);
+      System.out.println(var5);
         System.out.println("Testing lowercase:");
         System.out.println("UnHappy : " + lowerCase("UnHappy"));
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
@@ -34,7 +43,9 @@ public class MyString {
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         boolean isSub = false;
-        for (int i = 0; i < (str1.length() - str2.length() + 1) && !isSub; i++){
+        if (str2.length() == 0)
+        return false;
+        for (int i = 0; i < str1.length() && !isSub; i++){
             if (str2.charAt(0) == str1.charAt(i)){
                 isSub = true;
                 for (int j = 0; j < str2.length() && isSub; j++){
