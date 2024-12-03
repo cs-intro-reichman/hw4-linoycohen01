07595746d33f18821a80fee35a4c4f509025d01f
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-12-03 22:47:02 +0200

    15

--- MyString.java
+++ MyString.java
@@ -10,7 +10,7 @@ public class MyString {
         System.out.println("Testing contains:");
 
         System.out.println(contains("Our product will transform the market", "transform")); // true
-        System.out.println(contains("happy", "unhappy")); // false
+        System.out.println(contains("This blockchain-based solution will disrupt the industry", "disrupt")); // false
         System.out.println(contains("historical", "story")); // false
         System.out.println(contains("psychology", "psycho")); // true
         System.out.println(contains("personality", "son")); // true
@@ -37,7 +37,7 @@ public class MyString {
         boolean isSub = false;
         if (str2.length() == 0)
         return true;
-        for (int i = 0; i < str1.length() && !isSub; i++){
+        for (int i = 0; i < (str1.length() - str2.length() + 1) && !isSub; i++){
             if (str2.charAt(0) == str1.charAt(i)){
                 isSub = true;
                 for (int j = 0; j < str2.length() && isSub; j++){
